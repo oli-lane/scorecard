8755dbace3a648e0cdc1ceac49fcb65e1571e06f
oli-lane <markus.oliver.lane@gmail.com>
2022-07-24 15:17:08 -0700

    hi

---
+++ src/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Scoregrid = styled.div`
+    display: grid;
+    grid-template-columns: 30px 200px auto 100px;
+    grid-gap: 2px;
+    background-color: #2196F3
+`;
