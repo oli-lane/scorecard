8c625f590f43120bf70c6f85e4241e3a36186818
oli-lane <markus.oliver.lane@gmail.com>
2022-07-24 15:35:34 -0700

    style changes

--- src/styles.js
+++ src/styles.js
@@ -6,3 +6,21 @@ export const Scoregrid = styled.div`
     grid-gap: 2px;
     background-color: #2196F3
 `;
+
+export const Headercell = styled.div`
+    background = #ccc;
+    color: #282828;
+    padding: 4px;
+    text-align: center;
+`;
+
+export const Inputcell = styled.input`
+    padding: 0 4px:
+    :not(:focus) {
+        text-align: right;
+    }
+    : focus {
+        border: 1px solid #1581ba;
+        background-color: #e7f2f8
+    }
+`;
