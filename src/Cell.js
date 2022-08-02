f5af8076b9b2295600da28138c2d975188c35eb3
oli-lane <markus.oliver.lane@gmail.com>
2022-08-01 19:49:16 -0700

    total  updates mach 1

--- src/Cell.js
+++ src/Cell.js
@@ -1,7 +1,23 @@
 import React, {useCallback, useState, memo, useMemo, Component } from "react";
 import { Inputcell, Headercell } from "./styles";
 
-const Cell = ({rowIndex, columnIndex }) => {
+const Cell = ({rowIndex,columnIndex,setCellValue,currentValue,computeCell }) => {
+    const [edit, setEdit] = useState(false);
+
+    const value = useMemo(() => {
+        return computeCell({row: rowIndex});
+    }, [edit, computeCell, currentvalue, rowIndex]
+    );
+    
+    const handleEdit = useCallback(
+        change => {setCellValue({
+            row: rowIndex,
+            column: columnIndex,
+            value: change.target.value
+        });
+    }, 
+        [rowIndex, columnName, setCellValue]
+    );
     if (rowIndex == 0 && columnIndex == 0) {
         return <Headercell>Hole: </Headercell>
     }
@@ -16,10 +32,13 @@ const Cell = ({rowIndex, columnIndex }) => {
         return <Headercell>Player {rowIndex}</Headercell>
     }
     if (columnIndex == 19) {
-        return <Headercell>-</Headercell>
+        return <Headercell>value={value}</Headercell>
     }
     return (
-        <Inputcell/>
+        <Inputcell>
+            onChange={handleEdit}
+            type="text"
+        </Inputcell>
     );
 }
 
